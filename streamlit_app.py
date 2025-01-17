2eff40b7b6bdb4ce3c5132008d82cab1e3baa516
uki-snow <natashasample@gmail.com>
2025-01-16 20:44:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import panda as pd
+#import panda as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie! :cup_with_straw:")
